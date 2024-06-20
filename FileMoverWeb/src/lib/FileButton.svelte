442fa2a26a63e6b9085348eb77ec4d3b47026e0f
Sunil Rehman <sunil.rehman@gmail.com>
2024-06-20 16:20:19 -0700

    added tooltips

--- FileMoverWeb/src/lib/FileButton.svelte
+++ FileMoverWeb/src/lib/FileButton.svelte
@@ -3,18 +3,23 @@
     export let busy;
 </script>
 
-<button disabled='{busy}' on:click>
-    {#if ('DELETE' == Type)}
-    <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#384350" stroke-miterlimit="10" d="M2 4.5h20M10.5 8v10M13.5 8v10M7.43 21.5L5 4.5h13.52l-1.97 17H7.43z" style="stroke: #ffffff;"/><path fill="none" stroke="#384350" stroke-miterlimit="10" stroke-width="2" d="M9 3h6" style="stroke: #ffffff;"/></svg>
-
-    {:else if  ('COPYTORIGHT' == Type)}
-    <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#485664" stroke-miterlimit="10" d="M12 18l6-6-6-6M6 18l6-6-6-6" style="stroke: #ffffff;"/></svg>
+<div class="tooltip">
+    <button disabled='{busy}' on:click>
+        {#if ('DELETE' == Type)}
+        <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#384350" stroke-miterlimit="10" d="M2 4.5h20M10.5 8v10M13.5 8v10M7.43 21.5L5 4.5h13.52l-1.97 17H7.43z" style="stroke: #ffffff;"/><path fill="none" stroke="#384350" stroke-miterlimit="10" stroke-width="2" d="M9 3h6" style="stroke: #ffffff;"/></svg>
+        <span class="tooltiptext">Delete item</span>
     
-    {:else}
-    <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#485664" stroke-miterlimit="10" d="M12 18l-6-6 6-6M18 18l-6-6 6-6" style="stroke: #ffffff;"/></svg>
-
-    {/if}
-</button>
+        {:else if  ('COPYTORIGHT' == Type)}
+        <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#485664" stroke-miterlimit="10" d="M12 18l6-6-6-6M6 18l6-6-6-6" style="stroke: #ffffff;"/></svg>
+        <span class="tooltiptext">Copy item to right</span>
+        
+        {:else}
+        <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#485664" stroke-miterlimit="10" d="M12 18l-6-6 6-6M18 18l-6-6 6-6" style="stroke: #ffffff;"/></svg>
+        <span class="tooltiptext">Copy item to left</span>
+    
+        {/if}
+    </button>
+</div>
 
 <style>
 button {
@@ -43,6 +48,43 @@ button:hover {
 .svg-icon {
     width: 24px;
     height: 24px;
+}
+
+/* Basic styles for the tooltip container */
+.tooltip {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+}
+
+/* Hidden by default, the tooltip text */
+.tooltip .tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: rgb(255, 174, 0);
+    color: black;
+    text-align: center;
+    border-radius: 5px;
+    padding: 2px 0;
+    
+    /* Positioning the tooltip text */
+    position: absolute;
+    z-index: 1;
+    bottom: 100%; /* Position above the element */
+    left: 60%;
+    margin-left: -60px; /* Center the tooltip */
+    
+    /* Fade in tooltip */
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out, visibility 0s linear 1.5s;
+}
+
+/* Show the tooltip text when hovering */
+.tooltip:hover .tooltiptext {
+    visibility: visible;
+    opacity: 0.7;
+    transition: opacity 0.3s ease-in, visibility 0s linear;
+    transition-delay: 1s;
 }
 
 </style>
